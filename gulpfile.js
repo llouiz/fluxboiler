b5810d63bc38e9a80b016818dd19d7d94020453e
Luiz Carlos <luiz.carlos.techie@gmail.com>
2021-08-16 20:44:48 -0300

    structure changes

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var browserify = require('browserify');
 var reactify = require('reactify'); // Converts jsx to js
 var source = require('vinyl-source-stream'); // Converts string to a stream
 
-gulp.task('browserify', function () {
+gulp.task('browserify', async function () {
   browserify('./src/js/main.js')
     .transform('reactify')
     .bundle()
@@ -11,11 +11,12 @@ gulp.task('browserify', function () {
     .pipe(gulp.dest('dist/js'));
 });
 
-gulp.task('copy', function () {
+gulp.task('copy', async function () {
   gulp.src('src/index.html').pipe(gulp.dest('dist'));
+  gulp.src('src/css/*.*').pipe(gulp.dest('dist/css'));
   gulp.src('src/js/vendors/*.*').pipe(gulp.dest('dist/js'));
 });
 
-gulp.task('default', ['browserify', 'copy'], function () {
+gulp.task('default', gulp.series('browserify', 'copy'), async function () {
   return gulp.watch('src/**/*.*', ['browserify', 'copy']);
 });
